8e280491f085554284a9303cf0703cbfc0e27c7e
emiRod <L22390520@chetumal.tecnm.mx>
2025-03-30 19:37:32 -0500

    formulario pedidos con validacion html y js

---
+++ JavaScript/formEntrega.js
@@ -0,0 +1,29 @@
+//Hola José XD
+let regexCorreo = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+let regexNumTelefono = /^\+?[0-9]{1,3}[0-9]{7,}$/;
+
+// Seleccionamos el formulario
+let form = document.querySelector(".formulario");
+
+
+// Evento que se ejecuta cuando se envía el formulario
+form.addEventListener("submit", function(event) {
+    event.preventDefault(); // Evita que se recargue la página
+
+    // Obtener los valores de los campos
+    let numeroTel = document.querySelector(".noTelefono").value;
+    let correo = document.querySelector(".correo").value;
+
+    // Validar ambos campos
+    if (!regexCorreo.test(correo)) {
+        alert("El correo es inválido");
+        return;
+    }
+    if (!regexNumTelefono.test(numeroTel)) {
+        alert("El número de teléfono es inválido");
+        return;
+    }
+
+    alert("Datos correctos, formulario enviado.");
+    form.submit(); // Si todo está correcto, enviamos el formulario
+});
